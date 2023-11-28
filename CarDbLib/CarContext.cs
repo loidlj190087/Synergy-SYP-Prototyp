edd81f0e8f8d06e466cfb4e4b39089afc06d641d
Moritz Leuchtenmueller <LeuchtenmuellerM1981@sus.htl-grieskirchen.at>
2023-11-28 10:40:03 +0100

    Data seeding implemented

--- CarDbLib/CarContext.cs
+++ CarDbLib/CarContext.cs
@@ -5,22 +5,6 @@ public class CarContext
     public List<Car> Cars { get; set; } = new();
     public CarContext()
     {
-        var lines = File.ReadAllLines("cars.csv").Skip(1);
-        Car car;
-        foreach (var line in lines)
-        {
-            var values = line.Split(',');
-            car = new Car()
-            {
-                Id = int.Parse(values[0]),
-                Make = values[1],
-                Model = values[2],
-                YearOfManufacture = values[3],
-                Color = values[4],
-                NumberPlate = values[5],
-                Milage = int.Parse(values[6])
-            };
-            Cars.Add(car);
-        }
+        
     }
 }
--- CarPoolManager/MainWindow.xaml.cs
+++ CarPoolManager/MainWindow.xaml.cs
@@ -1,18 +1,42 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 using CarDbLib;
 namespace CarPoolManager;
 
 public partial class MainWindow : Window
 {
-    CarContext _cars;
+    CarContext _cars = new CarContext();
+    public List<Car> Cars { get; set; } = new();
     public MainWindow() => InitializeComponent();
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        _cars = new CarContext();
         cbbMakes.ItemsSource = _cars.Cars.Select(x => x.Make).ToList();
         cbbMakes.SelectedIndex = 0;
+    }
+
+    public void SeedDatabase()
+    {
+        _cars.Cars.Clear();
+        var lines = File.ReadAllLines("cars.csv").Skip(1);
+        Car car;
+        foreach (var line in lines)
+        {
+            var values = line.Split(',');
+            car = new Car()
+            {
+                Id = int.Parse(values[0]),
+                Make = values[1],
+                Model = values[2],
+                YearOfManufacture = values[3],
+                Color = values[4],
+                NumberPlate = values[5],
+                Milage = int.Parse(values[6])
+            };
+            Cars.Add(car);
+        }
+        _cars.Cars.AddRange(Cars);
     }
 
     private void txtId_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
