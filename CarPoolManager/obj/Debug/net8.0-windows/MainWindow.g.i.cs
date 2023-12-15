cb4fb91d05301fa0c7842eddfb1e8eda82e4d273
Moritz Leuchtenmueller <LeuchtenmuellerM1981@sus.htl-grieskirchen.at>
2023-12-15 10:26:55 +0100

    Sprint 1; realized; No errors

--- CarPoolManager/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ CarPoolManager/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -124,7 +124,7 @@ namespace CarPoolManager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CarPoolManager;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CarPoolManager;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
